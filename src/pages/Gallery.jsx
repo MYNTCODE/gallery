58bf32f6fffa64dbd22e527f43e0364164f7371b
myntp <119732035+MYNTCODE@users.noreply.github.com>
2023-10-10 02:17:22 +0700

    feat: add style videos container

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -6,12 +6,14 @@ function Gallery() {
       <h1> Gallery</h1>
       <section className="videos">
         {videos.map((video, index) => (
-          <div key={index} className="box">
-            <iframe title={video.title} src={video.video}></iframe>
+          <div key={index} className="container-videos">
+            <div className="box">
+              <iframe title={video.title} src={video.video}></iframe>
 
-            <p className="title" title="">
-              {video.title}
-            </p>
+              <p className="title" title={video.title}>
+                {video.title}
+              </p>
+            </div>
           </div>
         ))}
       </section>
